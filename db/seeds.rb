2141d5b6d97dbe99e7f2fe637cbf082ded02a440
Zhenni Zhou <zhennizhou6@gmail.com>
2022-11-18 13:58:58 +0800

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+puts "Cleaning database"
+Restaurant.destroy_all
+puts "Creating resaurants"
+5.times do
+  restaurant = Restaurant.create(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_address,
+    category: ['chinese', 'italian', 'japanese', 'french', 'belgian'].sample,
+    phone_number: Faker::PhoneNumber.cell_phone_with_country_code
+  )
+  puts "Restaurant with id: #{restaurant.id} has been created"
+end
+puts "Finished!"
