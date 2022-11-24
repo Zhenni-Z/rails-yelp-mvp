340869466dfdb16608c63391b6645af0106af729
Zhenni Zhou <zhennizhou6@gmail.com>
2022-11-24 15:22:14 +0800

    yelp

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  resources :restaurants
+  root to: "restaurants#index"
+  resources :restaurants, only: [:index, :show, :new, :create] do
+    resources :reviews, only: [:create, :index]
+  end
+end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-end
